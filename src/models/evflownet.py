5fbceb7a332e02e5dc274a53a95507faff3b5a35
kaisasaki-gif <sasaki.k.203@ms.saitama-u.ac.jp>
2024-07-17 17:04:36 +0900

    update

--- src/models/evflownet.py
+++ src/models/evflownet.py
@@ -7,30 +7,22 @@ _BASE_CHANNELS = 64
 
 class EVFlowNet(nn.Module):
     def __init__(self, args):
-        super(EVFlowNet,self).__init__()
+        super(EVFlowNet, self).__init__()
         self._args = args
 
-        self.encoder1 = general_conv2d(in_channels = 4, out_channels=_BASE_CHANNELS, do_batch_norm=not self._args.no_batch_norm)
-        self.encoder2 = general_conv2d(in_channels = _BASE_CHANNELS, out_channels=2*_BASE_CHANNELS, do_batch_norm=not self._args.no_batch_norm)
-        self.encoder3 = general_conv2d(in_channels = 2*_BASE_CHANNELS, out_channels=4*_BASE_CHANNELS, do_batch_norm=not self._args.no_batch_norm)
-        self.encoder4 = general_conv2d(in_channels = 4*_BASE_CHANNELS, out_channels=8*_BASE_CHANNELS, do_batch_norm=not self._args.no_batch_norm)
+        self.encoder1 = general_conv2d(in_channels=4, out_channels=_BASE_CHANNELS, do_batch_norm=not self._args.no_batch_norm)
+        self.encoder2 = general_conv2d(in_channels=_BASE_CHANNELS, out_channels=2*_BASE_CHANNELS, do_batch_norm=not self._args.no_batch_norm)
+        self.encoder3 = general_conv2d(in_channels=2*_BASE_CHANNELS, out_channels=4*_BASE_CHANNELS, do_batch_norm=not self._args.no_batch_norm)
+        self.encoder4 = general_conv2d(in_channels=4*_BASE_CHANNELS, out_channels=8*_BASE_CHANNELS, do_batch_norm=not self._args.no_batch_norm)
 
         self.resnet_block = nn.Sequential(*[build_resnet_block(8*_BASE_CHANNELS, do_batch_norm=not self._args.no_batch_norm) for i in range(2)])
 
-        self.decoder1 = upsample_conv2d_and_predict_flow(in_channels=16*_BASE_CHANNELS,
-                        out_channels=4*_BASE_CHANNELS, do_batch_norm=not self._args.no_batch_norm)
+        self.decoder1 = upsample_conv2d_and_predict_flow(in_channels=16*_BASE_CHANNELS, out_channels=4*_BASE_CHANNELS, do_batch_norm=not self._args.no_batch_norm)
+        self.decoder2 = upsample_conv2d_and_predict_flow(in_channels=8*_BASE_CHANNELS+2, out_channels=2*_BASE_CHANNELS, do_batch_norm=not self._args.no_batch_norm)
+        self.decoder3 = upsample_conv2d_and_predict_flow(in_channels=4*_BASE_CHANNELS+2, out_channels=_BASE_CHANNELS, do_batch_norm=not self._args.no_batch_norm)
+        self.decoder4 = upsample_conv2d_and_predict_flow(in_channels=2*_BASE_CHANNELS+2, out_channels=int(_BASE_CHANNELS/2), do_batch_norm=not self._args.no_batch_norm)
 
-        self.decoder2 = upsample_conv2d_and_predict_flow(in_channels=8*_BASE_CHANNELS+2,
-                        out_channels=2*_BASE_CHANNELS, do_batch_norm=not self._args.no_batch_norm)
-
-        self.decoder3 = upsample_conv2d_and_predict_flow(in_channels=4*_BASE_CHANNELS+2,
-                        out_channels=_BASE_CHANNELS, do_batch_norm=not self._args.no_batch_norm)
-
-        self.decoder4 = upsample_conv2d_and_predict_flow(in_channels=2*_BASE_CHANNELS+2,
-                        out_channels=int(_BASE_CHANNELS/2), do_batch_norm=not self._args.no_batch_norm)
-
-    def forward(self, inputs: Dict[str, Any]) -> Dict[str, Any]:
-        # encoder
+    def forward(self, inputs):
         skip_connections = {}
         inputs = self.encoder1(inputs)
         skip_connections['skip0'] = inputs.clone()
@@ -41,29 +33,26 @@ class EVFlowNet(nn.Module):
         inputs = self.encoder4(inputs)
         skip_connections['skip3'] = inputs.clone()
 
-        # transition
         inputs = self.resnet_block(inputs)
 
-        # decoder
-        flow_dict = {}
+        flows = []
         inputs = torch.cat([inputs, skip_connections['skip3']], dim=1)
         inputs, flow = self.decoder1(inputs)
-        flow_dict['flow0'] = flow.clone()
+        flows.append(flow.clone())
 
         inputs = torch.cat([inputs, skip_connections['skip2']], dim=1)
         inputs, flow = self.decoder2(inputs)
-        flow_dict['flow1'] = flow.clone()
+        flows.append(flow.clone())
 
         inputs = torch.cat([inputs, skip_connections['skip1']], dim=1)
         inputs, flow = self.decoder3(inputs)
-        flow_dict['flow2'] = flow.clone()
+        flows.append(flow.clone())
 
         inputs = torch.cat([inputs, skip_connections['skip0']], dim=1)
         inputs, flow = self.decoder4(inputs)
-        flow_dict['flow3'] = flow.clone()
+        flows.append(flow.clone())
 
-        return flow
-        
+        return flows
 
 # if __name__ == "__main__":
 #     from config import configs
